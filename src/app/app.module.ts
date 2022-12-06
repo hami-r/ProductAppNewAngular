e0c60a3510bd26b922ba6ce406687dc23586c638
hami-r <mynameissaitama54@gmail.com>
2022-12-06 22:02:32 +0530

    view page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { ViewProductComponent } from './view-product/view-product.component';
 
 let myRoutes:Routes = [
   {
     path:"",
     component:AddProductComponent
+  },
+  {
+    path:"view",
+    component:ViewProductComponent
   }
 ]
 @NgModule({
   declarations: [
     AppComponent,
-    AddProductComponent
+    AddProductComponent,
+    ViewProductComponent
   ],
   imports: [
     BrowserModule,

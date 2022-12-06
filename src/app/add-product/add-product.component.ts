fd4a8c8c9680e2d28ba685c1ee7995540afe2ce7
hami-r <mynameissaitama54@gmail.com>
2022-12-06 21:55:46 +0530

    add page with routing and databinding

---
+++ src/app/add-product/add-product.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-product',
+  templateUrl: './add-product.component.html',
+  styleUrls: ['./add-product.component.css']
+})
+export class AddProductComponent {
+    productCode = ""
+    name = ""
+    manufacturingDate = ""
+    expiryDate = ""
+    brand = ""
+    price = ""
+    seller = ""
+    distributor = ""
+
+    readValues =  () => {
+      
+    }
+}
